e6a03d54e5027785eab66a84db62488f236604e9
PK <pk@example.com>
2026-09-04 12:36:41 +0200

    Fixed names

--- map-link.js
+++ map-link.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  const isAppleDevice = /Macintosh|MacIntel|iPad|iPhone|iPod/.test(
+    `${navigator.userAgent} ${navigator.platform}`
+  );
   const mapLinks = document.querySelectorAll('a.map-link');
 
   mapLinks.forEach(link => {
     const appleUrl = link.dataset.apple;
     const googleUrl = link.dataset.google;
 
-    if (isIOS && appleUrl) {
+    if (isAppleDevice && appleUrl) {
       link.href = appleUrl;
     } else if (googleUrl) {
       link.href = googleUrl;
